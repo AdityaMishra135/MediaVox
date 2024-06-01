b843fc3d3ca3963a10fb332335bee01dc327f26c
Aditya Mishra <ak1355189@gmail.com>
2024-06-01 14:47:09 +0530

    feat: splash activity

--- app/src/main/kotlin/org/skywaves/mediavox/activities/SplashActivity.kt
+++ app/src/main/kotlin/org/skywaves/mediavox/activities/SplashActivity.kt
@@ -1,17 +1,35 @@
 package org.skywaves.mediavox.activities
 
+import android.annotation.SuppressLint
 import android.content.Intent
-import org.skywaves.mediavox.core.activities.BaseSplashActivity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import org.skywaves.mediavox.core.R
+import org.skywaves.mediavox.core.extensions.baseConfig
+import org.skywaves.mediavox.core.extensions.isUsingSystemDarkTheme
+import org.skywaves.mediavox.core.extensions.viewBinding
 import org.skywaves.mediavox.core.helpers.ensureBackgroundThread
+import org.skywaves.mediavox.databinding.ActivitySplashBinding
 import org.skywaves.mediavox.extensions.config
 import org.skywaves.mediavox.extensions.favoritesDB
 import org.skywaves.mediavox.extensions.getFavoriteFromPath
 import org.skywaves.mediavox.extensions.mediaDB
 import org.skywaves.mediavox.models.Favorite
 
-class SplashActivity : BaseSplashActivity() {
-    override fun initActivity() {
-        // check if previously selected favorite items have been properly migrated into the new Favorites table
+class SplashActivity : AppCompatActivity() {
+    private val binding by viewBinding(ActivitySplashBinding::inflate)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        baseConfig.apply {
+            if (isUsingAutoTheme) {
+                val isUsingSystemDarkTheme = isUsingSystemDarkTheme()
+                textColor = resources.getColor(if (isUsingSystemDarkTheme) R.color.theme_dark_text_color else R.color.theme_light_text_color)
+                backgroundColor = resources.getColor(if (isUsingSystemDarkTheme) R.color.theme_dark_background_color else R.color.theme_light_background_color)
+            }
+        }
+
         if (config.wereFavoritesMigrated) {
             launchActivity()
         } else {
